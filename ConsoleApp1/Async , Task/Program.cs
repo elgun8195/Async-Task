f6cfaab30ce99be28da8e9c2baf8258a2c17e17a
elgun <99824750+elgun8195@users.noreply.github.com>
2022-04-05 08:08:20 +0400

    created
    task

---
+++ ConsoleApp1/Async , Task/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async___Task
+{
+    class Program
+    {
+        public int Count { get; set; }
+        private Object object1 = new Object();
+        private Object object2 = new Object();
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(Loop1);
+            Thread thread2 = new Thread(Loop2);
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();            
+        }
+        #region Loops
+        static void Loop1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"Loop1 {i}");
+            }
+        }
+        static void Loop2()
+        {
+            for (int i = 100; i < 200; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"Loop2 {i}");
+            }
+        }
+        static void Loop3()
+        {
+            for (int i = 300; i < 400; i++)
+            {
+                Console.WriteLine($"Loop3 {i}");
+            }
+        }
+        #endregion
+
+        #region Medthods
+        void Increase()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                lock (object1)
+                {
+                    lock (object2)
+                    {
+                        Console.WriteLine(i);
+                        Count++;
+                    }
+                }
+
+
+            }
+        }
+        void Decrease()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                lock (object2)
+                {
+                    lock (object1)
+                    {
+                        Console.WriteLine(i);
+                        Count--;
+                    }
+                }
+
+            }
+        }
+
+        #endregion  
+
+        public static async Task<int> Sum()  // async metod geriye bir sey qaytarir ve onu await elemeliyik
+        {
+            int result = 0;
+            var task = Task.Run(() =>        // predicate isteyir bizden
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    result += i;
+                }
+            });
+            await task;
+            return result;
+        }
+        private async Task<int> Test(int count)
+        {
+            Console.WriteLine("eggs pan");
+            await Task.Delay(200);
+            Console.WriteLine($"{count}");
+            Console.WriteLine("cooking");
+            await Task.Delay(2000);
+            Console.WriteLine("plate");
+            return 5;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Models/Department.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    internal class Department : IEnumerable<Department>
+    {
+        private static int _id;
+        private List<Employee> _employee;
+        private Employee[] _emps;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //public Employee this[int index]
+        //{
+        //    get
+        //    {
+        //        if (index < _employee.Count)
+        //        {
+        //            return _employee[index];
+        //        }
+        //        throw new IndexOutOfRangeException("Partladi");
+        //    }
+        //    set
+        //    {
+        //        if (index < _employee.Count)
+        //        {
+
+
+        //            _employee[index] = value;
+        //            return;
+        //        }
+        //        throw new IndexOutOfRangeException("Partladi");
+        //    }
+        //}
+        public Department(string name)
+        {
+            _id++;
+            Id = _id;
+            Name = name;
+            _employee = new List<Employee>();
+            _emps = new Employee[0];
+        }
+
+
+        public void AddEmployee(Employee employee)
+        {
+            _employee.Add(employee);
+        }
+
+        public IEnumerator<Department> GetEnumerator()
+        {
+             _employee.GetEnumerator();
+            yield break;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
